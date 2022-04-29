1165bac3ea2bdabbe6454876d18dd5db9d683204
kryshyr <pbsaluria@up.edu.ph>
2022-04-29 21:47:07 +0800

    commit -m “Lecture 6 Assignment”

---
+++ Lecture6/Assignments/as5.c
@@ -0,0 +1,12 @@
+/* Program that checks the length of an array using sizeof operator. */
+
+#include<stdio.h>
+
+int main(void) {
+    int number[16];
+
+    size_t n = sizeof(number) / sizeof(number[0]);
+    printf("Total elements the array can hold is: %d\n", n);
+
+    return 0;
+} 
